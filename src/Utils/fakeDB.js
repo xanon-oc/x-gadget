f928016e31a1e442c90ab515de16acf7c2aadb3a
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-04-08 22:03:52 +0600

    added DataBase Function

---
+++ src/Utils/fakeDB.js
@@ -0,0 +1,22 @@
+// Add Data To Local Storage
+
+const addToDb = (id) => {
+  let shoppingCart = {};
+  // get previous data from local storage
+  const storedCart = localStorage.getItem("shopping-cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
+  }
+
+  // add quantity
+  const quantity = shoppingCart[id];
+  if (quantity) {
+    const newQuantity = quantity + 1;
+    shoppingCart[id] = newQuantity;
+  } else {
+    shoppingCart[id] = 1;
+  }
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
+};
+
+export default addToDb ;
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import ProductCard from "./Cards/ProductCard";
+import addToDb from "../Utils/fakeDB";
 
 const Shop = () => {
+  // card button handler
+
+  const handleAddToCart = (id) => {
+    addToDb(id);
+  };
+
   const productData = useLoaderData();
   return (
     <div className="product-container">
       {productData.map((product) => (
-        <ProductCard product={product} key={product.id} />
+        <ProductCard
+          handleAddToCart={handleAddToCart}
+          product={product}
+          key={product.id}
+        />
       ))}
     </div>
   );
